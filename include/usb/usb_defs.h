aea6b2885268a9c199b2e72c41a9ae48b48791a2
Joe Turner <joe@agavemountain.com>
2020-03-28 13:28:11 -0500

    Add HID Class-Specific Requests values
    Added HID class-specific request values, per 7.2 of HID
    specifications.

--- include/usb/usb_defs.h
+++ include/usb/usb_defs.h
@@ -21,3 +21,16 @@
 #pragma once
 #include <libusb.h>
 
+// HID Class-Specific Requests values. See section 7.2 of the HID specifications
+#define HID_GET_REPORT              0x01
+#define HID_GET_IDLE                0x02
+#define HID_GET_PROTOCOL            0x03
+#define HID_SET_REPORT              0x09
+#define HID_SET_IDLE                0x0A
+#define HID_SET_PROTOCOL            0x0B
+#define HID_REPORT_TYPE_INPUT       0x01
+#define HID_REPORT_TYPE_OUTPUT      0x02
+#define HID_REPORT_TYPE_FEATURE     0x03
+
+#define CTRL_IN			LIBUSB_ENDPOINT_IN |LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE
+#define CTRL_OUT		LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE
