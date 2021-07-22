47f1e95c3fb7791c4edb6cc992005598a1c2d44c
Joe Turner <joe@agavemountain.com>
2021-07-22 18:43:37 -0500

    Add hid and bulk transfer

--- include/usb/usb_device.h
+++ include/usb/usb_device.h
@@ -19,7 +19,7 @@
  */
 #pragma once
 #include <iostream>
-
+#include <libusb-1.0/libusb.h>
 #include "usb/usb_config_descriptor.h"
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #include "spdlog/sinks/stdout_sinks.h"
@@ -27,6 +27,8 @@
 
 namespace superflash {
 namespace usb {
+
+#define EP_IN	0x80
 
     class USBDevice {
 
@@ -68,7 +70,7 @@ namespace usb {
         {
             int rc = libusb_get_device_descriptor(_device, &_descriptor);
             if (rc < 0) {
-                std::cerr << "failed to get device descriptor\n";
+                spdlog::error("failed to get device descriptor");
             }
             return rc;
         }
@@ -82,7 +84,20 @@ namespace usb {
 
             return ret;
         }
+
+        int getVID() const
+        {
+            return _descriptor.idVendor;
+        }
+
+        int getPID() const
+        {
+            return _descriptor.idProduct;
+        }
+
         struct libusb_device* getDevice() const { return _device; }
+
+        libusb_device_handle* getHandle() const { return _handle; }
 
         void dump()
         {
@@ -160,11 +175,137 @@ namespace usb {
             libusb_close(_handle);
         }
 
-        int transfer_bulk();
-        int transfer_hid();
+        //! \brief Initiate bulk transfer
+        //!
+        //! Per imx_usb: 
+        //!
+        //!     For Bulk class drivers, the device is configured as
+        //!     EP0IN, EP0OUT control transfer
+        //!     EP1OUT - bulk out
+        //!     (max packet size of 512 bytes)
+        //!     EP2IN - bulk in
+        //!     (max packet size of 512 bytes)
+        //!
+        //! \param report
+        //! \param data
+        //! \param length
+        //! \param last
+        //! 
+        //! \returns
+        //! \retval 0 on success (and populates transferred) 
+        //! \retval LIBUSB_ERROR_TIMEOUT if the transfer timed out (and populates transferred) 
+        //! \retval LIBUSB_ERROR_PIPE if the endpoint halted 
+        //! \retval LIBUSB_ERROR_OVERFLOW if the device offered more data, see Packets and overflows 
+        //! \retval LIBUSB_ERROR_NO_DEVICE if the device has been disconnected 
+        //! \retval LIBUSB_ERROR_BUSY if called from event handling context 
+        //! \retval LIBUSB_ERROR_INVALID_PARAM if the transfer size is larger than the operating system and/or hardware can support (see Transfer length limitations) 
+        //!                                     another LIBUSB_ERROR code on other failures 
+        int transfer_bulk(int report, uint8_t *data, unsigned int length, int *last)
+        {
+            const int timeout = 1000;
+            int err;
+            *last = 0;
+            err = libusb_bulk_transfer(_handle,
+                (report < 3) ? 1 : 2 + EP_IN, data, length, last, timeout);
+        
+            return err;
+        }
+
+        //! \brief HID transfer
+        //!
+        //! Per imx_usb:
+        //!     For HID class drivers, 4 reports are used to implement
+        //!     Serial Download protocol(SDP)
+        //!     Report 1 (control out endpoint) 16 byte SDP comand
+        //!     (total of 17 bytes with 1st byte report id of 0x01
+        //!     Report 2 (control out endpoint) data associated with report 1 commands
+        //!     (max size of 1025 with 1st byte of 0x02)
+        //!     Report 3 (interrupt in endpoint) HAB security state
+        //!     (max size of 5 bytes with 1st byte of 0x03)
+        //!     (0x12343412 production)
+        //!     (0x56787856 engineering)
+        //!     Report 4 (interrupt in endpoint) date associated with report 1 commands
+        //!     (max size of 65 bytes with 1st byte of 0x04)
+        //!
+        //! \param report
+        //! \param data
+        //! \param length
+        //! \param last
+        //! 
+        int transfer_hid(int report, uint8_t *data, unsigned int length, int *last, bool use_ep1, int max_transfer)
+        {
+            const int timeout = 1000;
+            int err;
+            unsigned char tmp[1028];
+
+            SPDLOG_TRACE("report={}, cnt={:x}", report, length);
+            tmp[0] = (uint8_t)report;
+
+            if (report < 3)
+            {
+                memcpy(&tmp[1], data, length);
+                if (report == 2) {
+                    length = max_transfer;
+                }
+
+                if (!use_ep1)
+                {
+                    err = libusb_control_transfer(_handle,
+                        CTRL_OUT,
+                        HID_SET_REPORT,
+                        (HID_REPORT_TYPE_OUTPUT << 8) | report,
+                        0,
+                        tmp, length + 1, timeout);
+                    *last = (err > 0) ? err - 1 : 0;       
+                }
+                else
+                {
+                    *last = 0;
+                    err = libusb_interrupt_transfer(_handle, 1,
+                        tmp, length + 1, last, timeout);
+                    if (err < 0) {
+                        spdlog::error("{}}: read error({}) cnt={}, last_trans={}, {:02x} {:02x} {:02x} {:02x}",
+                            __func__, err,
+                            length, *last, data[0], data[1], data[2], data[3]);
+                    }
+                    if (*last)
+                        *last -= 1;
+                    SPDLOG_TRACE("{}: last_trans={}\n", __func__, *last);
+                }
+
+                if (err > 0) err = 0;
+            }
+            else
+            {
+                // Report 4 - interrupt in endpoint
+                *last = 0;
+                memset(&tmp[1], 0, length);
+                err = libusb_interrupt_transfer(_handle, 1 + EP_IN, tmp, length + 1, last, timeout);
+		        SPDLOG_TRACE("libusb_interrupt_transfer, err={}, trans={}", err, *last);
+                if (err >= 0)
+                {
+                    if (tmp[0] == (uint8_t) report) 
+                    {
+                        if (*last > 1) 
+                        {
+                            *last -= 1;
+                            memcpy(data, &tmp[1], *last);
+                        }
+                    }
+                    else
+                    {
+                        spdlog::warn("Unexpected report {} err={}, cnt={}, last_trans={}, {:02x} {:02x} {:02x} {:%02x}",
+                        tmp[0], err, length, *last, tmp[0], tmp[1], tmp[2], tmp[3]);
+                        err = 0;
+                    }
+                }
+
+            }
+            return err;
+        }
 
     private:
-        libusb_device_handle* _handle;
+        libusb_device_handle* _handle = NULL;
         struct libusb_device* _device = NULL;
         struct libusb_device_descriptor _descriptor;
     };
